0700d68d48e11d68cfad7547b24a7ad0083c91f6
steve6472 <20379161+steve6472@users.noreply.github.com>
2025-11-02 18:57:16 +0100

    Return removed stuff
    Took 37 minutes

--- src/main/java/steve6472/orbiter/world/ecs/components/physics/Collision.java
+++ src/main/java/steve6472/orbiter/world/ecs/components/physics/Collision.java
@@ -1,17 +1,17 @@
 package steve6472.orbiter.world.ecs.components.physics;
 
 import com.badlogic.ashley.core.Component;
-import com.github.stephengold.joltjni.BodyInterface;
 import com.github.stephengold.joltjni.Shape;
-import com.github.stephengold.joltjni.enumerate.EActivation;
 import com.mojang.serialization.Codec;
 import io.netty.buffer.ByteBuf;
 import steve6472.core.network.BufferCodec;
 import steve6472.core.network.BufferCodecs;
 import steve6472.core.registry.Key;
 import steve6472.orbiter.Constants;
-import steve6472.orbiter.OrbiterApp;
 import steve6472.orbiter.Registries;
+import steve6472.orbiter.world.collision.OrbiterCollisionShape;
+
+import java.util.Objects;
 
 /**
  * Created by steve6472
@@ -28,6 +28,8 @@ public record Collision(Key collisionKey, Shape shape) implements Component
 
     public Collision(Key collisionKey)
     {
-        this(collisionKey, Registries.COLLISION.get(collisionKey).collisionShape());
+        OrbiterCollisionShape orbiterCollisionShape = Registries.COLLISION.get(collisionKey);
+        Objects.requireNonNull(orbiterCollisionShape, "No shape found for key '%s'".formatted(collisionKey));
+        this(collisionKey, orbiterCollisionShape.collisionShape());
     }
 }
